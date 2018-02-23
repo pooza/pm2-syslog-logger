93180cbb1576864f027fee699a9b390df0794867
Tatsuya Koishi <pooza@kues.b-shock.local>
2018-02-23 17:15:49 +0900

    refs #1

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+const pm2 = require('pm2');
+const exec = require('child_process').exec;
+const shellescape = require('shell-escape');
+
+pm2.launchBus(function(err, bus) {
+  bus.on('log:err', function(data) {
+    exec([
+      'logger',
+      '-t', shellescape([data.process.name]),
+      shellescape([data.data]),
+    ].join(' '));
+  });
+
+  bus.on('log:out', function(data) {
+    exec([
+      'logger',
+      '-t', shellescape([data.process.name]),
+      shellescape([data.data]),
+    ].join(' '));
+  });
+});
