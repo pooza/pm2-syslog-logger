f94c1dc9407834653feddbd4ba70d5bf37b123e7
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-02-24 10:53:23 +0900

    fixes #3

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ const pm2 = require('pm2');
 const exec = require('child_process').exec;
 const shellescape = require('shell-escape');
 
-pm2.launchBus(function(err, bus) {
-  bus.on('log:err', function(data) {
+pm2.launchBus(function (err, bus) {
+  bus.on('log:err', function (data) {
     exec([
       'logger',
+      '-p', 'user.error',
       '-t', shellescape([data.process.name]),
       shellescape([data.data]),
     ].join(' '));
   });
 
-  bus.on('log:out', function(data) {
+  bus.on('log:out', function (data) {
     exec([
       'logger',
+      '-p', 'user.info',
       '-t', shellescape([data.process.name]),
       shellescape([data.data]),
     ].join(' '));
