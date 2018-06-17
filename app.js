8f38589743348e543bf2204248d21a8c8a237edd
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2018-06-17 12:03:44 +0900

    #11

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const posix = require('posix');
 pm2.launchBus((err, bus) => {
   bus.on('log:err', data => {
     posix.openlog(data.process.name, {ndelay: true, pid: true}, 'user');
-    posix.syslog('info', data.data);
+    posix.syslog('crit', data.data);
     posix.closelog();
   });
 
   bus.on('log:out', data => {
     posix.openlog(data.process.name, {ndelay: true, pid: true}, 'user');
-    posix.syslog('error', data.data);
+    posix.syslog('info', data.data);
     posix.closelog();
   });
 });
