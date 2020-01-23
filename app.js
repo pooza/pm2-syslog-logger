834b967a3043ff8c8e2bc4a7f27f83d06f03a4c5
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2020-01-23 17:49:17 +0900

    #18

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-'use strict';
-const pm2 = require('pm2');
-const posix = require('posix');
+'use strict'
+const pm2 = require('pm2')
+const posix = require('posix')
 
 pm2.launchBus((err, bus) => {
   bus.on('log:err', data => {
-    posix.openlog(data.process.name, {ndelay: true, pid: true}, 'user');
-    posix.syslog('crit', data.data);
-    posix.closelog();
-  });
+    posix.openlog(data.process.name, {ndelay: true, pid: true}, 'user')
+    posix.syslog('crit', data.data)
+    posix.closelog()
+  })
 
   bus.on('log:out', data => {
-    posix.openlog(data.process.name, {ndelay: true, pid: true}, 'user');
-    posix.syslog('info', data.data);
-    posix.closelog();
-  });
-});
+    posix.openlog(data.process.name, {ndelay: true, pid: true}, 'user')
+    posix.syslog('info', data.data)
+    posix.closelog()
+  })
+})
